b75854bac9776d190d024633dd88751c92221764
Nijat Namazzade <42466886+iamnijat@users.noreply.github.com>
2024-02-03 17:03:19 +0400

    Feat/ios fix (#3)
    * fixes in bottom navigation bar
    
    * fix: update iOS and Android configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
+import 'dart:io';
+
 import 'app_configs.dart';
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   await AppConfigs.config();
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
